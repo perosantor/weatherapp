6ce75effda9803577561c381ff4c7869974b0fae
Petar Santor <pero.santor@gmail.com>
2018-10-29 10:36:42 +0100

    MapViewController

---
+++ WeatherApp/ViewController/WeatherDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherDetailsViewController.swift
+//  WeatherApp
+//
+//  Created by Petar Santor on 10/29/18.
+//  Copyright © 2018 Apptisan. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
